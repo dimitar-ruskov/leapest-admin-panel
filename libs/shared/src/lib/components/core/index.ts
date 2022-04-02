312a01eecc333840bdca6814c18d9947ae708ba7
George <george.levchenko@edcast.com>
2022-04-02 13:01:51 +0300

    Index.ts files deleted to prevent importing all components as a bundle

--- libs/shared/src/lib/components/core/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './footer/footer.module';
-export * from './navbar/navbar.module';
-export * from './not-authenticated/not-authenticated.module';
-export * from './not-authorized/not-authorized.module';
-export * from './not-found/not-found.module';
